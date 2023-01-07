e2aee17ce13b8e9e23b4b5509ea01d408cf9a5a9
Boboe16 <114482350+Boboe16@users.noreply.github.com>
2023-01-08 00:21:48 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Is this script considered intermediate?
+
 function setting_button() {
     document.getElementById('container').style.visibility = 'visible'
     document.getElementById('start_button').style.visibility = 'hidden'
@@ -9,29 +11,49 @@ function hide_setting_to_show_start_button() {
 }
 
 function reset_URLS() {
-    let all_list = document.querySelectorAll('li')
-    for (let i = 0; i < 3; i++) {
-        all_list[i].remove()
+    try {
+        localStorage.clear()
+        let all_list = document.querySelectorAll('li')
+        for (let i = 0; i < 10; i++) {
+            all_list[i].remove()
+        }
+    } catch {
+        console.log('There is no URL stored in your storage.')
     }
-    localStorage.clear()
 }
 
 function open_all_URL() {
-    for (let i = 0; i < localStorage.length; i++) {
-        let keys = localStorage.key(i)
-        window.open(`${localStorage.getItem(keys)}`, '_blank')
-    }
-}
+    let numTabsToOpen = 10;  // number of tabs to open at a time
+    let delay = 1000;  // delay in milliseconds between batches of tab openings
+    let i = 0;
+    let openTabs = () => {
+      for (; i < localStorage.length && i < numTabsToOpen; i++) {
+        let keys = localStorage.key(i);
+        window.open(`${localStorage.getItem(keys)}`, '_blank');
+      }
+      if (i < localStorage.length) { // When the openTabs function is called, it will open a batch of tabs stored in local storage and then increment the i variable by 1 for each tab that is opened. After the openTabs function finishes executing, the if (i < localStorage.length) statement is evaluated again. If i is still less than the length of the local storage, the code inside the if statement will be executed again and the openTabs function will be called again after a delay of delay milliseconds.    
+        setTimeout(openTabs, delay); //This process continues until i is equal to the length of the local storage, at which point the if statement will not be executed anymore and the open_all_URL() function will end.
+      }                              // Open the next batch of tabs after a delay
+    };    
+    openTabs();
+  }
 
 show_saved_URL()
 
 function show_saved_URL() { // Show's the URLS by creating a li inside ul
-    for (let i = 0; i < localStorage.length; i++) {
-        let keys = localStorage.key(i)
-        let ul = document.getElementById('list')
-        let create_child_elements = document.createElement('li')
-        create_child_elements.innerHTML = `${localStorage.getItem(keys)}`
-        ul.appendChild(create_child_elements)
+    try {
+        let all_list = document.querySelectorAll('li')
+        for (let i = 0; i < 10; i++) {
+            all_list[i].remove()
+        }
+    }catch {
+        for (let i = 0; i < localStorage.length; i++) {
+            let keys = localStorage.key(i)
+            let ul = document.getElementById('list')
+            let create_child_elements = document.createElement('li')
+            create_child_elements.innerHTML = `${localStorage.getItem(keys)}`
+            ul.appendChild(create_child_elements)
+        }
     }
 }
 
@@ -79,8 +101,13 @@ async function save_data(URL) {
         }
 }
 
-function processForm() {
-    let URL = document.getElementById('name').value
-    save_data(URL)
-    show_saved_URL()
+function processForm(event) {
+    // if (localStorage.length == 3) {
+    //     return
+    // }
+    let URL = document.getElementById('name').value;
+    save_data(URL);
+    show_saved_URL();
+    document.getElementById('name').value = ''
+    event.preventDefault();
 }
